e31a5f870eed98489980fef1916e2160b2ba3043
Vasily Styazhkin <vasilyator@gmail.com>
2024-06-27 21:08:29 +0700

    mark revfella as not so alive

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,9 +3,9 @@ export default function ProjectsPage() {
     <main className="main-container d-flex align-items-center">
       <div className="container px-2">
         <div className="row justify-content-center gap-2">
-          <a className="col-12 col-md-6 card border p-2 flex-nowrap shadow-sm contact-card-item" href="https://revfella.com">
+          <a className="col-12 col-md-6 card border p-2 flex-nowrap shadow-sm contact-card-item" href="#">
             <div>
-              Motorcycle exhaust player: <span className="text-decoration-underline d-inline">RevFella.com</span>
+              (RIP) Motorcycle exhaust player: <span className="text-decoration-underline d-inline">RevFella.com</span>
             </div>
           </a>
         </div>
