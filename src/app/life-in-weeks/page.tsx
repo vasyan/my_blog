03464fd05a64bdbac8687410fb7e1e6bc6726e4e
Vasily Styazhkin <vasilyator@gmail.com>
2024-08-02 11:13:30 +0700

    Life dates update

--- src/app/life-in-weeks/page.tsx
+++ src/app/life-in-weeks/page.tsx
@@ -1,7 +1,30 @@
 import './styles.scss';
 
+interface LovelyDayInLife {
+  week: number,
+  label: string
+}
+
 const rows = Array(52).fill(Array(75).fill(0));
-const weeksPass = calculateElapsedTime();
+const weeksPass = calculateElapsedTime(new Date('1989-10-16'), new Date());
+const lovelyDays: LovelyDayInLife[] = [
+  {
+    week: weeksPass - calculateElapsedTime(new Date('2015-12-29')),
+    label: 'First time travel to Asia'
+  },
+  {
+    week: weeksPass - calculateElapsedTime(new Date('2013-11-03')),
+    label: 'First developer job landed'
+  },
+  {
+    week: weeksPass - calculateElapsedTime(new Date('2013-09-07')),
+    label: 'Yandex UI development school enrolled'
+  },
+  {
+    week: weeksPass - calculateElapsedTime(new Date('2020-03-13')),
+    label: 'Arrived to Thailand'
+  },
+];
 
 export default function LifeInWeeks() {
   return (
@@ -12,8 +35,13 @@ export default function LifeInWeeks() {
         </div>
         <div className="container life-in-weeks-container p-0">
           {rows.flat().map((v, index) => {
+            const lovelyDay = lovelyDays.find(v => v.week === index);
             return (
-              <input key={index} type="checkbox" checked={index < weeksPass} readOnly />
+              <input
+                key={index} type="checkbox" checked={index < weeksPass} readOnly
+                className={lovelyDay ? 'invert' : ''}
+                title={lovelyDay?.label}
+              />
             );
           })}
         </div>
@@ -23,15 +51,15 @@ export default function LifeInWeeks() {
 }
 
   // code is heavily inspired by https://github.com/bryanbraun/your-life/blob/gh-pages/your-life.js
-  function calculateElapsedTime() {
-    const currentDate = new Date();
-    const dateOfBirth = new Date('1989-10-16');
-    const diff = currentDate.getTime() - dateOfBirth.getTime();
+  function calculateElapsedTime(from: Date, to = new Date()) {
+    const toDate = to;
+    const fromDate = from;
+    const diff = toDate.getTime() - fromDate.getTime();
 
     const elapsedYears = (new Date(diff).getUTCFullYear() - 1970);
-    const isThisYearsBirthdayPassed = (currentDate.getTime() > new Date(currentDate.getUTCFullYear(), dateOfBirth.getMonth(), dateOfBirth.getDay()).getTime());
+    const isThisYearsBirthdayPassed = (toDate.getTime() > new Date(toDate.getUTCFullYear(), fromDate.getMonth(), fromDate.getDay()).getTime());
     const birthdayYearOffset = isThisYearsBirthdayPassed ? 0 : 1;
-    const dateOfLastBirthday = new Date(currentDate.getUTCFullYear() - birthdayYearOffset, dateOfBirth.getMonth(), dateOfBirth.getDay());
-    const elapsedDaysSinceLastBirthday = Math.floor((currentDate.getTime() - dateOfLastBirthday.getTime()) / (1000 * 60 * 60 * 24));
+    const dateOfLastBirthday = new Date(toDate.getUTCFullYear() - birthdayYearOffset, fromDate.getMonth(), fromDate.getDay());
+    const elapsedDaysSinceLastBirthday = Math.floor((toDate.getTime() - dateOfLastBirthday.getTime()) / (1000 * 60 * 60 * 24));
     return (elapsedYears * 52) + Math.floor(elapsedDaysSinceLastBirthday / 7);
   }
